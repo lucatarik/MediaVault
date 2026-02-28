e802a74afa964a638465037416927f127380b5b5
lucatarik <lucatarik@gmail.com>
2026-02-28 16:36:58 +0100

    Update extractor.js

--- js/extractor.js
+++ js/extractor.js
@@ -814,7 +814,7 @@ class _XHRResponse(io.RawIOBase):
 
         # ── Parsing headers con http.client.parse_headers ────────────────
         try:
-            CRLF = b"\r\n"
+            CRLF = bytes([13, 10])
             buf = io.BytesIO()
             for k, v in (proxy_resp.headers_dict or {}).items():
                 ks = str(k).strip()
